1ddeba0007946b702d983a36f6ee736ed9615112
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-01-15 18:03:11 +0000

    clearning up

--- headers/headers.go
+++ headers/headers.go
@@ -18,33 +18,33 @@ func main(){
 	fmt.Println("Headers.go")
 	req, err := http.NewRequest("GET", "https://httpbin.org/basic-auth/user/passw0rd", nil)
 	u.ErrNil(err, "Unable to create request")
-	
-	
+
+
 	buffer := &bytes.Buffer{}
-	
+
 	enc := base64.NewEncoder(base64.URLEncoding, buffer)
 	enc.Write([]byte("user:passw0rd"))
 	enc.Close()
-	
+
 	encodedCreds, err := buffer.ReadString('\n')
-	
-	if err != nil && err.Error() != "EOF" { 
+
+	if err != nil && err.Error() != "EOF" {
 		os.Exit(-1)
 	}
-	
+
     req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedCreds))
-	
+
 	resp, err := http.DefaultClient.Do(req)
 	u.ErrNil(err, "Failed to do request")
 	defer resp.Body.Close()
-	
+
 	content, err := ioutil.ReadAll(resp.Body)
 	u.ErrNil(err, "Unable to read body")
-	
+
 
     fmt.Println( string(content) )
     fmt.Println( resp.StatusCode )
-    
+
     u.V(version)
 }
 
