13025235d216bbc6c7d065182b8a8278f5c62f9b
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-01-19 15:46:39 +0000

    adding runs

--- tmp.go
+++ tmp.go
@@ -16,13 +16,12 @@ import (
 var version string = "0.0.1"
 
 func main() {
-    
     resp, err := http.Get(os.Args[1])
     u.ErrNil(err, "Unable to read response")
     defer resp.Body.Close()
     content, err := ioutil.ReadAll(resp.Body)
     u.ErrNil(err, "Unable to read content")
-    
+
     fmt.Println(string(content))
  // -----------------  footer ----------- //    
  u.V(version)
