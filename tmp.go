36496dac230428bdffa7559ea0aead89e19406d6
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-01-19 17:01:38 +0000

    adding graphpkg

--- tmp.go
+++ tmp.go
@@ -1,6 +1,9 @@
 package main
+// Help for developers:
 // go build -ldflags "-X main.version=0.0.1"
 // godoc -http :9090
+// root@ardeshir ~/httpdoc (master) $ graphpkg -stdout net > graph
+// ardeshir.org/graph to view the SVG of all dependencies 
 
 import (
 	"net/http"
