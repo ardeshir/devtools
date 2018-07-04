e1f23dbcb1a85d9475a6da6ab959782a3c64c784
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-07-04 20:56:40 +0000

    adding errors

---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+// Help for developers:
+// go build -ldflags "-X main.version=0.0.1"
+// godoc -http :9090
+// root@ardeshir ~/httpdoc (master) $ graphpkg -stdout net > graph
+// ardeshir.org/graph to view the SVG of all dependencies 
+
+import (
+	"net/http"
+	"flag"
+	// "encoding/json"
+	// "encoding/base64"
+	// "bytes"
+   "io/ioutil"
+   "os"
+	// "log"
+	"fmt"
+	"errors"
+   u "github.com/ardeshir/version"
+)
+
+var mymap = map[string]string{
+       "first": "Ardeshir",
+       "last": "Sepahsalar",
+       "age": "47",
+       "role": "Developer",
+       }
+
+type Person struct {
+    First string
+    Last string
+    Age  int 
+    Role string
+    Email string 
+}
+
+func (p Person) Salary() (int, error) {
+    
+      switch p.Role {
+          
+          case "Developer" : 
+             return 100, nil
+          case "Architect" : 
+             return 200, nil
+       }
+       return 0, errors.New( fmt.Sprintf("Unable to parse role: '%s' ", p.Role), )
+}
+
+func (p *Person) UpdateEmail(email string) {
+    p.Email = email;
+}
+
+
+func main() {
+    
+   ardeshir := Person{   First: "Ardeshir",  Last: "Sepahsalar", Age: 47, Role: "Developer" }
+   john     := Person{   First: "John",  Last: "Jupyter", Age: 70, Role: "Architect" }
+   jack     := Person{   First: "Jack",  Last: "Johnson", Age: 30, Role: "Designer" }
+   
+   aSal, erra :=  ardeshir.Salary();
+   if erra != nil {
+       fmt.Println(erra)
+   } else {
+         fmt.Println(ardeshir.First, aSal)
+   }
+   
+   jSal, errj :=  john.Salary();
+   if errj != nil {
+       fmt.Println(errj)
+   } else {
+       fmt.Println(john.First,jSal )
+   }
+   kSal, errk := jack.Salary();
+   if errk != nil {
+       fmt.Println(errk)
+   } else {
+      fmt.Println(jack.First, kSal) 
+   }
+   
+ 
+   
+   
+   
+   ardeshir.UpdateEmail("ardeshir.org@gmail.com")
+   fmt.Println(ardeshir.Email)
+   
+     first, ok := mymap["first"]
+
+     if ok == true {
+         fmt.Println("First:", first)
+     } else {
+         fmt.Println("First is not defined")
+     }
+
+    for key, value := range mymap {
+        fmt.Println("key: ", key, " value:", value)
+    }
+
+
+    url2get  := flag.String("url", defaultURL() , "Link to download")
+    flag.Parse()
+
+    // resp, err := http.Get(os.Args[1])
+
+    resp, err := http.Get(*url2get)
+    u.ErrNil(err, "Unable to read response")
+    defer resp.Body.Close()
+    content, err := ioutil.ReadAll(resp.Body)
+    u.ErrNil(err, "Unable to read content")
+
+    fmt.Println(string(content))
+
+
+ // --------- basic housekeeping ----------- // 
+ // if we're int debug mode, print out info 
+
+   if debugTrue() {
+    u.V( defaultVersion() )
+   }
+}
+
+// Function to check env variable DEFAULT_DEBUG bool
+func debugTrue() bool {
+
+     if os.Getenv("DEFAULT_DEBUG") != "" {
+        return true
+     }
+     return false
+}
+
+// Function to check env variable DEFAULT_VERSION string
+func defaultVersion() string {
+
+ if os.Getenv("DEFAULT_VERSION") != "" {
+     return os.Getenv("DEFAULT_VERSION")
+  }
+
+    var version string = "0.0.1"
+ return version
+}
+
+// Function to check env variable DEFAULT_URL to http get
+func defaultURL() string {
+    if os.Getenv("DEFAULT_URL") != "" {
+        return os.Getenv("DEFAULT_URL")
+    }
+    return "https://httpbin.org/get"
+}
